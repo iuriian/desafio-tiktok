f055869f93ab3ef366cb0e480650d3a57e0c154a
iuriian <iuriian@hotmail.com>
2019-12-10 01:46:43 -0300

    ajuste de responsivo nos cards e no body de vido

--- src/app/content-options/content-options.component.ts
+++ src/app/content-options/content-options.component.ts
@@ -1,10 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'content-options',
   templateUrl: './content-options.component.html',
-  styleUrls: ['./content-options.component.scss']
+  styleUrls: ['./content-options.component.scss'],
+  animations: [
+    trigger('cardByCard', [
+      state('void', style({
+        opacity: 0,
+        transform: 'perspective(500px) translateZ(-200px)'
+      })),
+      state('*', style({
+        opacity: 1,
+        transform: 'perspective(500px) translateZ(0px)'
+      })),
+      transition(':enter', [
+        animate('2s ease', style({
+          opacity: 1,
+          transform: 'perspective(500px) translateZ(0px)'
+        }))
+      ]),
+      transition(':leave', [
+        animate('2s ease', style({
+          opacity: 0,
+          transform: 'perspective(500px) translateZ(-200px)'
+        }))
+      ])
+    ])
+  ]
 })
 export class ContentOptionsComponent implements OnInit {
 
